4e5a88bc9fcbab933d619d78fef882d236eae48c
WilliamKelly00 <williammkelly00@gmail.com>
2022-04-03 16:24:21 -0400

    initial commit

---
+++ src/components/SignIn.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <!-- create sign in, email password -->
+        <h2> email </h2>
+        <h2> password </h2>
+        <button>Sign In</button>
+        <button @click="Increment">{{ count }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignIn',
+    props: {
+        msg: String,
+    },
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    methods: {
+        Increment() {
+            this.count++;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
